1496324dbe74937a662f1637dad2ab117e8a2987
165rmv <47989205+165rmv@users.noreply.github.com>
2022-02-16 21:13:33 -0600

    lo nuevo

--- Email.java
+++ Email.java
@@ -3,4 +3,12 @@ private final dataBase db;
 private User user; 
 private String msg; 
 
+public void login(String username, String password){
+
+}
+
+public void sendemail(User user, String msg){
+    
+}
+
 }
